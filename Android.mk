2cec6ac7177752bfea3121f2b69398a98ad99cdd
Aleksandrl39 <40720446+Aleksandrl39@users.noreply.github.com>
2025-11-17 09:30:24 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
+LOCAL_SHARED_LIBRARIES := \
+    libfscrypttwrp  # ← добавить эту строку
+
 ifeq ($(TARGET_DEVICE),X663)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
